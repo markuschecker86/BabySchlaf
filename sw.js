951abb1f7bcb4bd6f0aee2df82c40ffbbeee3a37
Audio Craft <markus.grossmann86@gmail.com>
2026-02-18 21:21:20 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+
 const CACHE='babyschlaf-v3';
 const ASSETS=['/','/index.html','/manifest.json','/icons/icon-192.png','/icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
